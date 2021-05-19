34a97380dfde844ec1349eca4611f49c4601423b
Namya503 <76729199+Namya503@users.noreply.github.com>
2021-05-19 21:28:24 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,9 +9,9 @@ const Body = Matter.Body;
 
 function preload()
 {
-	starImg = loadImage("images/star.png");
-	bgImg = loadImage("images/starNight.png");
-	fairyImg=loadAnimation("images/fairyImage1.png","images/fairyImage2.png")
+	starImg = loadImage("star.png");
+	bgImg = loadImage("starNight.png");
+	fairyImg=loadAnimation("fairyImage1.png","fairyImage2.png")
 	fairyvoice=loadSound("sound/Joymusic.mp3");
 }
 
